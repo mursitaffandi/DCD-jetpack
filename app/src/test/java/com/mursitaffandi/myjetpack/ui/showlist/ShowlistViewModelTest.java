1d3f40f30b7971e466bad7d768aca2f74f7c87e5
mursitaffandi <mursitaffandi@gmail.com>
2019-07-26 10:49:15 +0700

    init

---
+++ app/src/test/java/com/mursitaffandi/myjetpack/ui/showlist/ShowlistViewModelTest.java
@@ -0,0 +1,16 @@
+package com.mursitaffandi.myjetpack.ui.showlist;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ShowlistViewModelTest {
+
+    @Test
+    public void getMovies() {
+    }
+
+    @Test
+    public void getTvShows() {
+    }
+}
